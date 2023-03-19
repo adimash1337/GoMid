9fd2bcb8700607b7f5fd0d9eaf0d3e8a4a5de9e8
adimash1337 <dimash.amurzakov@gmail.com>
2023-03-19 20:22:13 +0600

    unfinished ver of midterm

--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,31 @@ package routes
 
 import (
 	"github.com/adimash1337/GoMid/controllers"
+	"github.com/adimash1337/GoMid/database"
+
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/users/signup", controllers.SignUp())
 	incomingRoutes.POST("/users/login", controllers.Login())
-	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewAdmin())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
 	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
 	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}
+
+func CartRoutes(incomingRoutes *gin.Engine) {
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+	incomingRoutes.GET("/addtocart", app.AddToCart())
+	incomingRoutes.GET("/removeitem", app.RemoveItem())
+	incomingRoutes.GET("/listcart", controllers.GetItemFromCart())
+	incomingRoutes.GET("/cartcheckout", app.BuyFromCart())
+	incomingRoutes.GET("/instantbuy", app.InstantBuy())
+}
+
+func AddressRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/address/add", controllers.AddAddress())
+	incomingRoutes.PUT("/address/edithome", controllers.EditHomeAddress())
+	incomingRoutes.PUT("/address/editwork", controllers.EditWorkAddress())
+	incomingRoutes.GET("/address/delete", controllers.DeleteAddress())
 }
