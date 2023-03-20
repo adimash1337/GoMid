6373e4ffd1e4e428568d530f3a60d85f803c7342
adimash1337 <dimash.amurzakov@gmail.com>
2023-03-20 22:36:46 +0600

    midterm1 final v2

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -86,9 +86,6 @@ func SignUp() gin.HandlerFunc {
 		token, refreshtoken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
 		user.Token = &token
 		user.Refresh_Token = &refreshtoken
-		user.UserCart = make([]models.ProductUser, 0)
-		user.Address_Details = make([]models.Address, 0)
-		user.Order_Status = make([]models.Order, 0)
 		_, inserterr := UserCollection.InsertOne(ctx, user)
 		if inserterr != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "not created"})
--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"github.com/adimash1337/GoMid/controllers"
-	"github.com/adimash1337/GoMid/database"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,23 +12,4 @@ func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin()) //+
 	incomingRoutes.GET("/users/productview", controllers.SearchProduct())      //+
 	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())    //+
-	//
-	incomingRoutes.POST("/address/add", controllers.AddAddress())          //+
-	incomingRoutes.PUT("/address/edithome", controllers.EditHomeAddress()) //+
-	incomingRoutes.PUT("/address/editwork", controllers.EditWorkAddress()) //+
-	incomingRoutes.GET("/address/delete", controllers.DeleteAddress())     //+
-	//
-	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
-	incomingRoutes.GET("/addtocart", app.AddToCart())              //+
-	incomingRoutes.GET("/removeitem", app.RemoveItem())            //+
-	incomingRoutes.GET("/listcart", controllers.GetItemFromCart()) //+
-	incomingRoutes.GET("/cartcheckout", app.BuyFromCart())         //+
-	incomingRoutes.GET("/instantbuy", app.InstantBuy())            //+
 }
-
-//func CartRoutes(incomingRoutes *gin.Engine) {
-//}
-//
-//func AddressRoutes(incomingRoutes *gin.Engine) {
-//
-//}
